5c6bcdf724b6f86f322bc086fe8ab0994c92b06d
nitinthomas20 <nitinthomas20@gmail.com>
2026-01-11 03:43:25 +1100

    chore: integrate Firebase and Firestore into Angular application
    - Added Firebase and AngularFire dependencies in package.json.
    - Configured Firebase initialization and Firestore provision in app.config.ts.
    - Defined environment variables for Firebase configuration in environment.ts.

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+  apiKey: "AIzaSyCviAPEdI4Pk9AxBgI8Ioo2H6CmhCg-06E",
+  authDomain: "geogame-0001.firebaseapp.com",
+  projectId: "geogame-0001",
+  storageBucket: "geogame-0001.firebasestorage.app",
+  messagingSenderId: "605654199834",
+  appId: "1:605654199834:web:4921cbb30f74333ebfaba3"
+}
+};
